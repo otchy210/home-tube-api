25f9b366e287a9c4a8c642b012d1a3e7c85235f7
Otchy <id+github@otchy.net>
2021-12-26 13:41:38 +0900

    0.0.17: Add start script

---
+++ bin/start.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+"use strict";
+const { ApiServer } = require('../dist');
+
+const port = process.env.HOME_TUBE_API_PORT || 8210;
+const apiServer = new ApiServer({port});
+apiServer.start().then(() => {
+    const appConfigPath = apiServer.getAppConfigPath();
+    console.log('==== Home Tube API Server ======================================')
+    console.log(`Running on http://localhost:${port}`);
+    console.log(`appConfigPath: ${appConfigPath}`);
+    console.log('Press Ctrl+C to stop the server');
+    console.log('================================================================')
+});
