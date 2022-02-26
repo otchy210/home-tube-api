59bed0b53fb7f63ac475590da6d348c3bfc5067f
Otchy <id+github@otchy.net>
2022-02-26 18:02:08 +0900

    0.10.8: Accept Buffer in hashing functions

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,9 +1,9 @@
 import { createHash } from 'crypto';
 
-export const md5 = (str: string): string => {
+export const md5 = (str: string | Buffer): string => {
     return createHash('md5').update(str).digest('hex');
 };
 
-export const sha256 = (str: string): string => {
+export const sha256 = (str: string | Buffer): string => {
     return createHash('sha256').update(str).digest('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
 };
