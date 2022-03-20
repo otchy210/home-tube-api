39c8d8313e1a909ad3975c6dda903a34113510b2
Otchy <id+github@otchy.net>
2022-03-21 00:25:07 +0900

    0.12.4: Remove unnecessary test

--- src/utils/NetworkUtils.test.ts
+++
@@ -1,7 +0,0 @@
-import { getLocalIpv4Addresses } from './NetworkUtils';
-
-describe('NetworkUtils', () => {
-    it('getLocalIpv4Addresses', () => {
-        console.log(getLocalIpv4Addresses());
-    });
-});
